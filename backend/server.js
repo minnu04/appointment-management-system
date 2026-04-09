8a8940d96a7da7594dee21439c09151447b93778
minnu04 <tadikondasrivardhini9@gmail.com>
2026-04-09 11:41:55 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ const corsOptions = {
 app.use(
   cors(corsOptions),
 );
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true }));
 
